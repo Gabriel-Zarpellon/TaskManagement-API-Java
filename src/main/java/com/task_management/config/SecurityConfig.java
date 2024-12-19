7fab91a6e87389e526c9f89da2418b91a9cbb62e
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-12-19 15:25:22 -0300

    feat: add task  and user permissions

--- src/main/java/com/task_management/config/SecurityConfig.java
+++ src/main/java/com/task_management/config/SecurityConfig.java
@@ -7,18 +7,27 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.task_management.security.JwtTokenAuthenticationFilter;
+import com.task_management.security.JwtTokenService;
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http, JwtTokenService tokenService) throws Exception {
+        var tokenFilter = new JwtTokenAuthenticationFilter(tokenService);
+
         return http
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(a -> a
                         .requestMatchers(HttpMethod.POST, "/api/users/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/users/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "api/tasks/**").permitAll()
                         .anyRequest().authenticated())
+                .addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
